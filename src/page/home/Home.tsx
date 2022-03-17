4ca50a7307f542371edefef457fd35ecf44c0d65
David IOS <davidandrade@funcef.com.br>
2022-03-17 11:47:28 -0300

    botão de download

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -3,7 +3,12 @@ import {
     View,
     Container,
     Divider,
+    Button,
+    Link,
+    HStack,
+    Text,
 } from "native-base";
+import { BsDownload } from "react-icons/bs";
 import {
     ToggleDarkMode
 } from '../../component/ToggleDarkMode';
@@ -28,13 +33,18 @@ const Home = () => {
             <Container bg={colorMode === "light" ? "coolGray.200" : "darkBlue.900"}
                 minHeight="100vh" alignItems="center" p={4} minWidth="100%">
 
-                <View p={5}>
-                    <ToggleDarkMode />
-                </View>
-
                 <View bg={colorMode === "light" ? "coolGray.50" : "darkBlue.800"}
-                    rounded={10} width="900" maxWidth="90vw" shadow={5} p={[4, 8, 12]}
+                    rounded={10} width="900" maxWidth="90vw" shadow={5} p={[4, 8, 12]} pt={[2, 4, 8]}
                     justifyContent="center">
+                    <HStack flex={1} justifyContent="space-between" p={5}>
+                        <ToggleDarkMode />
+
+                        <Link href="https://drive.google.com/file/d/1eh8BTdhKPn8ls2W3az9d6w_fqkXjQEgv/view?usp=sharing" isExternal>
+                            <Button colorScheme="trueGray" leftIcon={<BsDownload color={colorMode === "light" ? "black" : "white"} />} variant="outline">
+                                <Text>Baixar Currículo</Text>
+                            </Button>
+                        </Link>
+                    </HStack>
                     <View>
                         <Header />
                         <Divider my={divider} />
--- src/page/home/partial/Header.tsx
+++ src/page/home/partial/Header.tsx
@@ -10,6 +10,7 @@ import {
     useColorMode,
 } from "native-base";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { IoLogoWhatsapp } from 'react-icons/io';
 
 const Header = () => {
     const { colorMode } = useColorMode();
@@ -22,14 +23,17 @@ const Header = () => {
                 <Box pl={[4, 4]} py={[0, 0]} flexBasis={0} flexGrow={1}>
                     <VStack w="100%">
                         <Text fontSize={['xl', '2xl']} lineHeight="md" flexWrap="wrap">David Jefferson</Text>
-                        <Text fontSize={['sm', 'lg']} lineHeight="md" flexWrap="wrap">Desenvolvedor Full Stack Web e Mobile Sênior.</Text>
+                        <Text fontSize={['sm', 'lg']} fontWeight="normal" lineHeight="md" flexWrap="wrap">Desenvolvedor Full Stack Web e Mobile Sênior.</Text>
                     </VStack>
                     <HStack flex={1}>
                         <Link mt={2} mr={2} href="https://www.linkedin.com/in/davidjeffersonbx/" isExternal>
                             <FaLinkedin color={color} size={30} />
                         </Link>
-                        <Link mt={2} href="https://github.com/davidjefferson" isExternal>
+                        <Link mt={2} mr={2} href="https://github.com/davidjefferson" isExternal>
                             <FaGithub color={color} size={30} />
+                        </Link>
+                        <Link mt={2} href="https://api.whatsapp.com/send?1=pt_br&phone=5561982968140" isExternal>
+                            <IoLogoWhatsapp color={color} size={30} />
                         </Link>
                     </HStack>
                 </Box>
